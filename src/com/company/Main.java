0784e7f9f50563720e04a2f2f06ebd6066fe9464
Lucía Vidal Ortiz <opalo97230704@gmail.com>
2022-02-15 12:56:28 +0100

    acabar

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.print("0 ");
+            for (int j = 0; j <= 9; j++) {
+                if (i > 0 && i < 9) {
+                    System.out.print("");
+                } else {
+                    System.out.print("0 ");
+                    }
+
+            } System.out.println();
+
+
+
+        }
+    }
+}
