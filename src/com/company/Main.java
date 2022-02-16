3cb70f5384687e15c6200a98518a3b82b46f9cc6
Lucía Vidal Ortiz <opalo97230704@gmail.com>
2022-02-16 09:36:54 +0100

    terminado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        int lado = 10;
+
+        for (int i = 0; i < lado; i++) {
             System.out.print("0 ");
-            for (int j = 0; j <= 9; j++) {
-                if (i > 0 && i < 9) {
-                    System.out.print("");
+            for (int j = 0; j < lado - 2; j++) {
+                if (i > 0 && i < lado - 1) {
+                    System.out.print("  ");
                 } else {
                     System.out.print("0 ");
-                    }
-
-            } System.out.println();
-
-
-
+                }
+            }
+            System.out.println("0");
         }
+
+        // Solución de Román
+        /*for (int i = 0; i < lado; i++) {
+            for (int j = 0; j < lado; j++) {
+                if (i == 0 || j == 0 || i == lado - 1 || j == lado - 1) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }*/
     }
 }
